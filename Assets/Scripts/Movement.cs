48b207e3d68fa602b91e0d62f1f375059bfdb10e
ercutto <ercumentti81@gmail.com>
2024-06-17 15:29:11 +0300

    Player  rotation to ground

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,9 +12,11 @@ public class Movement : MonoBehaviour
     public Rigidbody rb;
     RaycastHit hitFront;
     RaycastHit hitBack;
+    RaycastHit hit;
     public Transform groundCheck;
     public LayerMask layerMask;
     public LayerMask groundMask;
+    private bool isIdle;
 
     public GameObject playerGraphics=null;
     
@@ -40,8 +42,8 @@ public class Movement : MonoBehaviour
    
     bool isJumping=false;
     float horizontal;
-    float groundRotation = 0;
-    
+    float playerRotationY=0f;
+    Vector3 groundRotation;
 
     private void Awake()
     {
@@ -49,45 +51,15 @@ public class Movement : MonoBehaviour
     }
     private void Update()
     {
-       
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(-transform.up), out hit, Mathf.Infinity, layerMask))
-        {
-          
-            transform.rotation = hit.transform.rotation;
-           
-            isGroundedFront = true;
-            Debug.DrawRay(transform.position, transform.TransformDirection(-transform.up) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
-        }
-        else
-        {   isGroundedFront=false;
-            Debug.DrawRay(transform.position, transform.TransformDirection(-transform.up) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-        }
-
         
-        if (horizontal<0)
-        { 
-           
-          playerGraphics.transform.eulerAngles=new Vector3(0,-180,0);
-            
-        }else if (horizontal>0)
-        {
-            
-           playerGraphics.transform.eulerAngles=new Vector3(0,0,0);
-
-        }
 
         Walk();
         if (isGroundedFront)
         {
+           
 
-            Vector3.ProjectOnPlane(transform.position, hit.normal);
-
-            //rb.transform.position += new Vector3(horizontal*moveSpeedMutiplier,0,0);
-
+            
+           
             if (!onWall)
             {
                 if (Input.GetKeyDown(KeyCode.Space))
@@ -99,43 +71,49 @@ public class Movement : MonoBehaviour
             }
         }
 
-        //if (transform.rotation.eulerAngles.z > 30f || transform.rotation.eulerAngles.z < -2f){ onWall = true; }
-        //else { onWall = false; }
+      
+       
     }
     void Walk()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        
+
         transform.Translate(new Vector3(horizontal*moveSpeedMutiplier*Time.deltaTime, 0f, 0f));
-        if (horizontal < 0)
-        {
-
-           transform.eulerAngles = new Vector3(0, -180, 0);
-
-        }
-        
-        if (horizontal > 0)
-        {
-
-            transform.eulerAngles = new Vector3(0, 0, 0);
-
-        }
 
 
     }
+  
+
+    
     private void FixedUpdate()
     {
-      
-       
+        if (Physics.Raycast(transform.position, transform.TransformDirection(-transform.up), out hit, raycastRange, layerMask))
+        {
+
+           
+            isGroundedFront = true;
+            Quaternion grndTilt = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            
+            transform.rotation=Quaternion.Lerp(transform.rotation, grndTilt,Time.deltaTime*5f);
+            Debug.DrawRay(transform.position, transform.TransformDirection(-hit.normal) * raycastRange, Color.blue);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            isGroundedFront = false;
+            Debug.DrawRay(transform.position, transform.TransformDirection(-transform.up) * raycastRange, Color.red);
+            Debug.Log("Did not Hit");
+        }
+
     }
     void Jump()
     {
-        rb.AddForce(Vector3.right*jumpVelocity,ForceMode.Impulse);
+        //rb.AddForce(transform.forward*jumpVelocity,ForceMode.Impulse);
         rb.AddForce(Vector3.up*jumpVelocity,ForceMode.Impulse);
     }
     void OnWallJump()
     {
-        rb.AddForce(Vector3.right * jumpVelocity, ForceMode.Impulse);
+        rb.AddForce(transform.forward * jumpVelocity, ForceMode.Impulse);
         rb.AddForce(Vector3.up * jumpVelocity*3f, ForceMode.Impulse);
     }
     
